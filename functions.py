80b900c6fbdb95edb3bf5a0c3877e488f401eee3
Ederporto <eder.porto@example.com>
2021-09-09 16:56:51 -0300

    Adding signature

--- functions.py
+++ functions.py
@@ -9,6 +9,9 @@ from fpdf import FPDF
 
 
 class CertificationPDF(FPDF):
+    """
+    Class for the PDF to be made
+    """
     def header(self):
         pass
 
@@ -17,6 +20,12 @@ class CertificationPDF(FPDF):
 
 
 def clean_string(text):
+    """
+    Function to clean strings of invalid filename characters
+
+    :param text: String to be cleaned
+    :return: String with invalid filename characters removed
+    """
     invalid_characters = '\\/:*?"<>|'
     for ic in invalid_characters:
         text = text.replace(ic, "")
@@ -24,16 +33,30 @@ def clean_string(text):
 
 
 def role(host, pronoun):
+    """
+    Function to build the part of the certificate text that
+    identifies the type of certificate, if the person was a
+    host for the event or not
+
+    :param host: true|verdadeiro/false|falso if the person was a host or not
+    :param pronoun: the pronoun of the person, so a better writing can be made
+    :return: string stating if the person was a host or not
+    """
     if host == "verdadeiro" or host == "true":
         if pronoun.lower() == "a":
-            return "palestrante convidada"
+            return " como palestrante convidada"
         else:
-            return "palestrante convidado"
+            return " como palestrante convidado"
     else:
-        return "ouvinte"
+        return ""
 
 
 def check_hour(hour):
+    """
+    Function to validate the hour formatting
+    :param hour: string to be checked
+    :return: True/False depending if the string follows the HHhMM format
+    """
     try:
         if re.compile(r"\d{2}h\d{2}").match(hour):
             return True
@@ -44,6 +67,11 @@ def check_hour(hour):
 
 
 def check_pronoun(pronoun):
+    """
+    Function to validate the pronoun formatting
+    :param pronoun: string to be checked
+    :return: True/False depending if the string follows the A/O format
+    """
     pronouns = ['a', 'o']
     try:
         if not pronoun.lower() in pronouns:
@@ -54,6 +82,11 @@ def check_pronoun(pronoun):
 
 
 def check_date(date_text):
+    """
+    Function to validate the date formatting
+    :param date_text: string to be checked
+    :return: True/False depending if the string follows the dd/mm/yyyy format
+    """
     try:
         datetime.strptime(date_text, '%d/%m/%Y')
     except ValueError:
@@ -62,6 +95,11 @@ def check_date(date_text):
 
 
 def check_host(host):
+    """
+    Function to validate the host formatting
+    :param host: string to be checked
+    :return: True/False depending if the string follows the verdadeiro|true/falso|false format
+    """
     possible_values = ["verdadeiro", "falso", "true", "false"]
 
     try:
@@ -73,6 +111,11 @@ def check_host(host):
 
 
 def tf_host(host):
+    """
+    Function to turn the host string into a boolean value
+    :param host: string with the host status
+    :return: True/False
+    """
     positive_values = ["verdadeiro", "true"]
     if host.lower() in positive_values:
         return True
@@ -81,6 +124,11 @@ def tf_host(host):
 
 
 def check_columns(table):
+    """
+
+    :param table:
+    :return:
+    """
     columns = ["name", "username", "pronoun", "event", "date", "hours"]
     for col in columns:
         if col not in list(table.columns):
@@ -99,26 +147,26 @@ def make_pdf_of_user(user, app):
     #######################################################################################################
     # Header
     #######################################################################################################
-    pdf.set_y(20)  # Start the letter text at the 10x42mm point
+    pdf.set_y(15)  # Start the letter text at the 10x42mm point
 
     pdf.add_font('Merriweather', '', os.path.join(app.static_folder, 'fonts/Merriweather-Regular.ttf'), uni=True)
     pdf.add_font('Merriweather-Bold', '', os.path.join(app.static_folder, 'fonts/Merriweather-Bold.ttf'), uni=True)
-    pdf.set_font('Merriweather', '', 37)  # Text of the body in Times New Roman, regular, 13 pt
+    pdf.set_font('Merriweather', '', 35)  # Text of the body in Times New Roman, regular, 13 pt
 
     locale.setlocale(locale.LC_TIME, "pt_BR")  # Setting the language to portuguese for the date
     pdf.cell(w=0, h=10, border=0, ln=1, align='C', txt='CERTIFICADO')
 
-    pdf.set_font('Merriweather', '', 14.5)
-    pdf.cell(w=0, h=10, ln=1)  # New line
-    pdf.cell(w=0, h=10, border=0, ln=1, align='C', txt='O grupo de usuários Wiki Movimento Brasil '
-                                                       '(CNPJ 29.801.908/0001-86), certifica que')
-    pdf.cell(w=0, h=10, ln=1)  # New line
+    pdf.set_font('Merriweather', '', 13)
+    pdf.cell(w=0, h=5, ln=1)  # New line
+    pdf.cell(w=0, h=5, border=0, ln=1, align='C', txt='O grupo de usuários Wiki Movimento Brasil '
+                                                       '(CNPJ 29.801.908/0001-86) certifica que')
+    pdf.cell(w=0, h=5, ln=1)  # New line
 
     #######################################################################################################
     # User name
     #######################################################################################################
     name = user.name  # User full name
-    pdf.set_font('Merriweather', '', 35)
+    pdf.set_font('Merriweather', '', 30)
     name_size = pdf.get_string_width(name)
 
     if name_size > 287:
@@ -138,38 +186,45 @@ def make_pdf_of_user(user, app):
             pdf.set_font('Merriweather', '', math.floor(287 * 35 / name_size))
 
     pdf.cell(w=0, h=10, border=0, ln=1, align='C', txt=name)
-    pdf.cell(w=0, h=10, ln=1)  # New line
+    pdf.cell(w=0, h=5, ln=1)  # New line
 
     #######################################################################################################
     # por ter completado as leituras e as 6 tarefas do curso online
     #######################################################################################################
-    pdf.set_font('Merriweather', '', 14.5)
-    phrase_participation = "participou como " + role(user.host, user.pronoun) + " do evento"
-    pdf.cell(w=0, h=10, border=0, ln=1, align='C', txt=phrase_participation)
-    pdf.cell(w=0, h=8, ln=1)  # New line
+    pdf.set_font('Merriweather', '', 13)
+    phrase_participation = "participou" + role(user.host, user.pronoun) + " do evento"
+    pdf.cell(w=0, h=5, border=0, ln=1, align='C', txt=phrase_participation)
+    pdf.cell(w=0, h=5, ln=1)  # New line
 
     #######################################################################################################
     # Introdução ao Jornalismo Científico
     #######################################################################################################
-    pdf.set_font('Merriweather-Bold', '', 21)
+    pdf.set_font('Merriweather-Bold', '', 20)
     pdf.cell(w=0, h=10, border=0, ln=1, align='C', txt=user.event)
-    pdf.cell(w=0, h=8, ln=1)  # New line
+    pdf.cell(w=0, h=5, ln=1)  # New line
 
     #######################################################################################################
     # no dia X (Carga horária: Y horas)
     #######################################################################################################
-    pdf.set_font('Merriweather', '', 14.5)
+    pdf.set_font('Merriweather', '', 13)
     date = datetime.strptime(user.date, "%d/%m/%Y").date()
     phrase_time = "no dia " + date.strftime("%d de %B de %Y") + " (Carga horária: " + user.hours + ")."
-    pdf.cell(w=0, h=10, border=0, ln=1, align='C', txt=phrase_time)
-    pdf.cell(w=0, h=8, ln=1)  # New line
+    pdf.cell(w=0, h=5, border=0, ln=1, align='C', txt=phrase_time)
+    pdf.cell(w=0, h=15, ln=1)  # New line
+
+    y = pdf.get_y()
+    pdf.image(os.path.join(app.static_folder, 'jap.png'), x=131, y=y-5, w=35, h=16)
+    pdf.cell(w=0, h=5, border=0, ln=1, align='C', txt="______________________")
+    pdf.cell(w=0, h=10, border=0, ln=1, align='C', txt="JOÃO ALEXANDRE PESCHANSKI")
+    pdf.set_font('Merriweather', '', 11)
+    pdf.cell(w=0, h=5, border=0, ln=1, align='C', txt="Presidente do Wiki Movimento Brasil")
 
     user_hash = hashlib.sha1(
         bytes("Certificate " + user.name + user.event + user.hours + str(user.host), 'utf-8')).hexdigest()
     pdf.in_footer = 1
     pdf.set_y(-16.5)
     pdf.set_font('Merriweather', '', 8.8)
-    pdf.cell(w=0, h=6.5, border=0, ln=1, align='C',
+    pdf.cell(w=0, h=5, border=0, ln=1, align='C',
              txt='A validade deste documento pode ser checada em https://wmb.toolforge.org/. '
                  'O código de validação é:' + user_hash)
     pdf.in_footer = 0
